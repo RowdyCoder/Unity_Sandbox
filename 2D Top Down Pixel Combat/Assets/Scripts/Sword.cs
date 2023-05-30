144e9f41e8b13b7a543162f13e5148aba31c20c6
RowdyCoder <anthonyrayhollis@gmail.com>
2023-05-30 18:06:03 -0400

    Sword Swing Both Directions

--- 2D Top Down Pixel Combat/Assets/Scripts/Sword.cs
+++ 2D Top Down Pixel Combat/Assets/Scripts/Sword.cs
@@ -6,9 +6,13 @@ public class Sword : MonoBehaviour
 {
     [SerializeField] Animator myAnimator;
     private PlayerControls playerControls;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
 
     private void Awake()
     {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
         playerControls = new PlayerControls();
     }
 
@@ -18,10 +22,15 @@ public class Sword : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         //subscribe to attack function
         playerControls.Combat.Attack.started += _ => Attack();
+    }
+
+    private void Update()
+    {
+        MouseFollowWithOffset();
     }
 
     private void Attack()
@@ -30,9 +39,22 @@ public class Sword : MonoBehaviour
         myAnimator.SetTrigger("Attack");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MouseFollowWithOffset()
     {
-        
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+        } 
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
+
+ 
 }
