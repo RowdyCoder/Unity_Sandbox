a79695e9ab69bc07b32479ef4c934fe9344a2733
RowdyCoder <anthonyrayhollis@gmail.com>
2023-05-31 16:29:19 -0400

    Sword Collision

---
+++ 2D Top Down Pixel Combat/Assets/Scripts/DamageSource.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DamageSource : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.GetComponent<EnemyAI>())
+        {
+            Debug.Log("Enemy Collision occured");
+        }
+    }
+}
+
